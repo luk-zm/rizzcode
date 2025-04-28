f371b06caaaeb9d54ca06256af816ca72114066c
avgsurfman <genericairconditionerno.169@gmail.com>
2025-04-28 09:31:06 +0200

    More models work

--- zadania/models.py
+++ zadania/models.py
@@ -16,10 +16,25 @@ class Comment(models.Model):
         GOOD = 4
         RIZZ = 5
     rating = models.IntegerField(choices=Rating)
+    pub_date = models.DateTimeField("Date Published")
 
 
 class Exercise(models.Model):
     user = models.OnetoOneField(User, on_delete=models.CASCADE)
     completed = models.BooleanField(default=False)
     solution = models.CharField(max_lenght=10000)
+    pub_date = models.DateTimeField("Date Added")
+    LANG = {
+        "C#": "Csharp",
+        "Py": "Python",
+        "SQL": "Structured Query Language (SQL)"
+    }
+
+
+class Test(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE) # is this correct?
+    assert_output = models.CharField(max_lenght=500)
+
+    def __str__(self):
+        return self.assert_output
 

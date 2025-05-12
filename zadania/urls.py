9eeea8205c710f6bb65d30dd39145509d71e946e
avgsurfman <genericairconditionerno.169@gmail.com>
2025-05-12 17:43:38 +0200

    some urls added

--- zadania/urls.py
+++ zadania/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "zadania"
 
 urlpatterns = [
     path("", views.index, name="index"),
     # strona glowna zadan
+    path("py", views.python, name="python"),
+    path("py/<int:zadanie_id>", views.python_zadanie, name="python_zadanie"),
+    path("sql", views.sql, name="sql"),
+
 
 ]
